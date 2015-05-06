9c84f68ae4dfef9b2e74672aa85e81592a480ea1
hadley <h.wickham@gmail.com>
2015-05-06 11:04:01 -0500

    Code fixes for 1-data and 3-classify

--- 1-data/wine.r
+++ 1-data/wine.r
@@ -1,26 +1,31 @@
 library(ggplot2)
-ggopt(grid.fill = "grey95")
-source("~/documents/reshape/reshape/load.r")
-
-wine <- read.csv("../1-data/wine.csv")
-winer <- rescaler(wine, "range")
-
 library(rggobi)
+
+theme_update(panel.background = element_rect(fill = "grey95", colour = NA))
+
+rescale01 <- function(x) {
+  rng <- range(x, na.rm = TRUE)
+  (x - rng[1]) / (rng[2] - rng[1])
+}
+rescaler <- function(df) {
+  is_numeric <- vapply(df, is.numeric, logical(1))
+  df[is_numeric] <- lapply(df[is_numeric], rescale01)
+  df
+}
+
+# Load wine data and create version rescaled to 0-1
+wine <- read.csv("1-data/wine.csv")
+winer <- rescaler(wine)
+
 wgobi <- function(...) {
   g <- ggobi(...)
-  glyph_colour(g[[1]]) <- c(1, 5, 6)[wine$type]  
+  glyph_colour(g[[1]]) <- c(1, 5, 6)[wine$type]
   invisible(g)
 }
-wgobi(wine)
 
 col <- scale_colour_manual("Variety", values = c("#984EA3", "#4DAF4A", "#FF7F00"))
-fill <- scale_fill_manual("Variety", values = alpha(c("#984EA3", "#4DAF4A", "#FF7F00"), 0.3))
+fill <- scale_fill_manual("Variety", values = scales::alpha(c("#984EA3", "#4DAF4A", "#FF7F00"), 0.3))
 
 recolour <- function(g = ggobi_get()[[1]]) {
-  glyph_colour(g) <- c(1, 5, 6)[g$type]  #glyph_colour(g) + 1  
+  glyph_colour(g) <- c(1, 5, 6)[g$type]  #glyph_colour(g) + 1
 }
-winefly <- function(..., formula = type ~ color + phenols + flavanoids, n=1e5) {
-  print(classifly(wine, formula, ..., n=1e5))
-  recolour()
-}
-
--- 3-classifly/classifly.r
+++ 3-classifly/classifly.r
@@ -1,32 +1,46 @@
 library(ggplot2)
 library(classifly)
+library(MASS)
+library(randomForest)
+library(e1071)
 
-source("../1-data/wine.r")
+source("1-data/wine.r")
+winefly <- function(..., formula = type ~ color + phenols + flavanoids, n = 1e5) {
+  print(classifly(wine, formula, ..., n = 1e5))
+  recolour()
+}
 
-qplot(color, phenols, data=wine, colour=type) + col
+qplot(color, phenols, data = wine, colour = type) + col
 
-wine_lda <- classifly(winer, type ~ color + phenols, lda, n = 100 * 100, method="grid")
+wine_lda <- classifly(winer, type ~ color + phenols, lda, n = 250 * 250,
+  method = "grid")
 grid <- subset(wine_lda, .TYPE == "simulated")
 
-ggplot(grid, aes(color, phenols)) + geom_tile(aes(fill = type), colour=NA) + geom_point(aes(colour=type), data=winer) + col + fill + coord_equal()
-ggsave(file = "class-2d-shade.pdf", width=5, height=4)
+ggplot(grid, aes(color, phenols)) +
+  geom_raster(aes(fill = type)) +
+  geom_point(aes(colour = type), data = winer) +
+  col +
+  fill +
+  coord_equal()
+ggsave(file = "3-classifly/class-2d-shade.pdf", width = 5, height = 4)
 
-ggplot(grid, aes(color, phenols, colour=type)) + geom_point(data=winer) + geom_contour(data = grid, aes(x=color, y=phenols, z=.ADVANTAGE, colour=type), size = 0.5, breaks=0.05) + col + coord_equal() #+ scale_z_continuous(breaks=0.05) 
-ggsave(file = "class-2d-boundary.pdf", width=5, height=4)
+ggplot(grid, aes(color, phenols, colour = type)) +
+  geom_point(data = winer) +
+  geom_contour(data = grid, aes(z = .ADVANTAGE, colour = type),
+    size = 1, breaks = 0.10) +
+  col +
+  coord_equal()
+ggsave(file = "3-classifly/class-2d-boundary.pdf", width = 5, height = 4)
 
-
-w <- rescaler(read.csv("../6-nnet/wiggly.csv")[, -1], "range")
+w <- rescaler(read.csv("6-nnet/wiggly.csv")[, -1])
 w$class <- factor(w$class)
 classifly(w, class ~ ., randomForest)
 
 winefly(lda)
-winefly(svm, probability=T, kernel="linear")
+winefly(svm, probability = TRUE, kernel = "linear")
 
 f5 <- type ~ color + phenols + flavanoids + proline + dilution
-winefly(svm, probability=T, kernel="polynomial", formula = f5, n=1e6)
-
-winefly(svm, probability=T, kernel="radial")
-
-
+winefly(svm, probability = TRUE, kernel = "polynomial", formula = f5, n = 1e6)
+winefly(svm, probability = TRUE, kernel = "radial")
 
 # Random forests don't give clean boundaries
