fa10151c75894dff1dea5546901dffe911a73fbc
hadley <h.wickham@gmail.com>
2015-05-06 11:18:52 -0500

    Updates for 3-hclust

--- 3-hclust/hclust.r
+++ 3-hclust/hclust.r
@@ -1,19 +1,47 @@
-library(reshape)
 library(clusterfly)
+source("1-data/wine.r")
 
-source("../1-data/wine.r")
+# ggplot2 dengrograms ----------------------------------------------------------
 
+gdendro <- function(ward2) {
+  wardd <- ward2$data
+  wardd$type <- factor(c(as.character(wine$type), rep(NA, nrow(wine) - 1)))
+  vars <- c("ORDER", "POINTS", "type")
 
-sngl2 <- hierfly(winer[, -1], method="single")
-ward2 <- hierfly(winer[, -1], method="ward")
+  leaves <- subset(wardd, node == 0)
+  nodes <- subset(wardd, node == 1)
 
-source("dendro.r")
+  # < 0 = observation, > 0 = cluster
+  merge <- ward2$hclust$merge
+  merge[merge > 0] <- merge[merge > 0] + nrow(leaves)
+  merge[merge < 0] <- abs(merge[merge < 0])
+
+  lines <- data.frame(rbind(
+    cbind(nodes[, vars], wardd[merge[, 1], vars]),
+    cbind(nodes[, vars], wardd[merge[, 2], vars])
+  ))
+
+  ggplot(NULL, aes(ORDER, POINTS)) +
+    geom_point(aes(colour = type), data = leaves) +
+    geom_point(data = nodes, size = 0.5) +
+    geom_segment(aes(xend = ORDER.1, yend = POINTS.1, group = 1), data = lines) +
+    scale_x_continuous("",breaks = NULL) +
+    scale_y_continuous("Number of points in cluster") +
+    col
+}
+
+# Explore wine data -----------------------------------------------------------
+
+sngl2 <- hierfly(winer[, -1], method = "single")
+ward2 <- hierfly(winer[, -1], method = "ward.D")
+
+source("3-hclust/dendro.r")
 gdendro(ward2)
-ggsave(file = "wine-ward.pdf", width=6, height=4)
+ggsave("3-hclust/wine-ward.pdf", width = 6, height = 4)
 gdendro(sngl2)
-ggsave(file = "wine-single.pdf", width=6, height=4)
+ggsave("3-hclust/wine-single.pdf", width = 6, height = 4)
 
-g <- ggobi(sngl2)
+# g <- ggobi(sngl2)
 g <- ggobi(ward2)
-glyph_colour(g[[1]]) <- c(1, 5, 6)[wine$type]  
+glyph_colour(g[[1]]) <- c(1, 5, 6)[wine$type]
 # Open a tour of the variables, and a dendrogram using points vs order
--- 3-hclust/plots.r
+++ 3-hclust/plots.r
@@ -1,19 +1,28 @@
 library(DescribeDisplay)
 
-d<-dd_load("wine-hcward-1.r")
-d$plots[[1]]$points$cex[d$plots[[1]]$points$pch==18]<-0.01
-d$plots[[1]]$points$cex[d$plots[[1]]$points$pch==16]<-0.3
-d$plots[[1]]$points$col[d$plots[[1]]$points$pch==18]<-"#808080"
+d <- dd_load("3-hclust/wine-hcward-1.r")
+d$plots[[1]]$points$cex[d$plots[[1]]$points$pch == 18] <- 0.01
+d$plots[[1]]$points$cex[d$plots[[1]]$points$pch == 16] <- 0.3
+d$plots[[1]]$points$col[d$plots[[1]]$points$pch == 18] <- "#808080"
+
+pdf("3-hclust/wine-hcward-1.pdf", width = 5.5, height = 2)
 plot(d)
+dev.off()
 
-d<-dd_load("wine-hcward-2.r")
-d$plots[[1]]$points$cex[d$plots[[1]]$points$pch==18]<-0.01
-d$plots[[1]]$points$cex[d$plots[[1]]$points$pch==16]<-0.5
-d$plots[[1]]$points$col[d$plots[[1]]$points$pch==18]<-"#808080"
-plot(d, axislocation=c(0.85,0.15))
+d <- dd_load("3-hclust/wine-hcward-2.r")
+d$plots[[1]]$points$cex[d$plots[[1]]$points$pch == 18] <- 0.01
+d$plots[[1]]$points$cex[d$plots[[1]]$points$pch == 16] <- 0.5
+d$plots[[1]]$points$col[d$plots[[1]]$points$pch == 18] <- "#808080"
 
-d<-dd_load("wine-hcward-3.r")
-d$plots[[1]]$points$cex[d$plots[[1]]$points$pch==18]<-0.01
-d$plots[[1]]$points$cex[d$plots[[1]]$points$pch==16]<-0.5
-d$plots[[1]]$points$col[d$plots[[1]]$points$pch==18]<-"#808080"
-plot(d, axislocation=c(0.15,0.15))
+pdf("3-hclust/wine-hcward-2.pdf", width = 3, height = 3)
+plot(d, axislocation = c(0.2, 0.15))
+dev.off()
+
+d <- dd_load("3-hclust/wine-hcward-3.r")
+d$plots[[1]]$points$cex[d$plots[[1]]$points$pch == 18] <- 0.01
+d$plots[[1]]$points$cex[d$plots[[1]]$points$pch == 16] <- 0.5
+d$plots[[1]]$points$col[d$plots[[1]]$points$pch == 18] <- "#808080"
+
+pdf("3-hclust/wine-hcward-3.pdf", width = 3, height = 3)
+plot(d, axislocation = c(0.2, 0.15))
+dev.off()
