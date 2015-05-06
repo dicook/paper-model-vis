6973d616dfe034b62c9b94a5937432f92a7333ef
hadley <h.wickham@gmail.com>
2015-05-06 13:39:54 -0500

    Fixes for 3-manova

--- 3-manova/plots.r
+++ 3-manova/plots.r
@@ -1,13 +1,12 @@
 library(DescribeDisplay)
 
-d1 <- dd_load("manova-1.r")
-d2 <- dd_load("manova-2.r")
+d1 <- dd_load("3-manova/manova-1.r")
+d2 <- dd_load("3-manova/manova-2.r")
 
-png("manova-1.png", width = 5, height = 5, units = "in", res = 150)
-plot(d1, axislocation=c(0.2,0.15))
+png("3-manova/manova-1.png", width = 5, height = 5, units = "in", res = 150)
+plot(d1, axislocation = c(0.2, 0.15))
 dev.off()
 
-png("manova-2.png", width = 5, height = 5, units = "in", res = 150)
-plot(d2, axislocation=c(0.2,0.15))
+png("3-manova/manova-2.png", width = 5, height = 5, units = "in", res = 150)
+plot(d2, axislocation = c(0.2, 0.15))
 dev.off()
-
