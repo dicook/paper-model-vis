974f9be33417cef4a6f68cf35a799f85843655df
hadley <h.wickham@gmail.com>
2015-05-08 15:32:25 -0500

    Update 6-nnet.

--- 6-nnet/nnet.r
+++ 6-nnet/nnet.r
@@ -1,65 +1,83 @@
 library(ggplot2)
-theme_update(grid.fill = "grey95")
+library(nnet)
+library(plyr)
+theme_update(panel.background = element_rect(fill = "grey95", colour = NA))
 
-w <- read.csv("wiggly.csv")[ ,2:4]
+w <- read.csv("6-nnet/wiggly.csv")[ ,2:4]
 w <- transform(w, class = factor(class, labels = c("A", "B")))
 
-sqr <- opts(aspect.ratio = 1)
-nolab <- c(scale_x_continuous(""), scale_y_continuous(""))
+sqr <- theme(aspect.ratio = 1)
+nolab <- list(scale_x_continuous(""), scale_y_continuous(""))
 col_d <- scale_colour_manual(values = c("#377EB8", "#E41A1C"))
-fill_c <- scale_fill_gradient2(low = "#E41A1C", mid="white", high = "#377EB8", midpoint = 0.5)
-fill_c$train(c(0, 1))
+fill_c <- scale_fill_gradient2(low = "#E41A1C", mid = "white", high = "#377EB8",
+  midpoint = 0.5)
+# fill_c$train(c(0, 1))
 
-source("explore.r")
-load("wiggly-multi.rdata")
+source("6-nnet/explore.r")
+(load("6-nnet/wiggly-multi.rdata"))
 many <- rbind(many2$coef, many3$coef, many4$coef)
-many <- rename(many, c(size = "nodes"))
+many <- plyr::rename(many, c(size = "nodes"))
 many$id <- (many$nodes - 2) * 200 + as.integer(many$i)
 
 # Plot sample data -----------------------------------------------------------
 
-qplot(x, y, data=w, colour=class, shape=class, xlab="", ylab="") + sqr + col_d + nolab
-ggsave(file = "nnet-test.pdf", width = 6, height = 5)
+qplot(x, y, data = w, colour = class, shape = class, xlab = "", ylab = "") +
+  sqr +
+  col_d +
+  nolab
+ggsave("6-nnet/nnet-test.pdf", width = 6, height = 5)
 
 # Display model in data space ------------------------------------------------
 
 best <- Filter(function(x) accuracy(x) == 1, many4$all)[[1]]
 
-ggplot(data=subset(best$output,  node == 1), aes(x, y)) + geom_tile(aes(fill=pred)) + geom_point(aes(shape=class), data=w) + sqr + fill_c + nolab
+ggplot(subset(best$output,  node == 1), aes(x, y)) +
+  geom_raster(aes(fill = pred)) +
+  geom_point(aes(shape = class), data = w) +
+  sqr +
+  fill_c +
+  nolab
+ggsave("6-nnet/nnet-best-fill.pdf", width = 6, height = 5)
 
-ggsave(file = "nnet-best-fill.pdf", width=6, height=5)
-
-ggplot(data=subset(best$output,  node == 1), aes(x, y)) +
-  geom_point(aes(shape=class, colour=class), data=w) + sqr + col_d + 
-  geom_contour(aes(z=pred), breaks = c(0.1, 0.25, 0.5, 0.75, 0.9)) + nolab
-
-ggsave(file = "nnet-best-contour.pdf", width=6, height=5)
+ggplot(subset(best$output, node == 1), aes(x, y)) +
+  geom_point(aes(shape = class, colour = class), data = w) +
+  sqr +
+  col_d +
+  geom_contour(aes(z = pred), breaks = c(0.1, 0.25, 0.5, 0.75, 0.9)) +
+  nolab
+ggsave("6-nnet/nnet-best-contour.pdf", width = 6, height = 5)
 
 # Display hidden nodes -------------------------------------------------------
 
-hidden_fill <- scale_fill_gradient2(low="#AF8DC3", mid="#F7F7F7", high="#7FBF7B", midpoint=0.5)
-hidden_fill$train(c(0, 1))
+hidden_fill <- scale_fill_gradient2(low = "#AF8DC3", mid = "#F7F7F7",
+  high = "#7FBF7B", midpoint = 0.5, limits = c(0, 1))
 
-ggplot(data=best$hidden, aes(x, y)) + geom_tile(aes(fill=pred)) + geom_point(aes(shape=class), data=w) +sqr + facet_grid(. ~ node) + hidden_fill  + nolab
+ggplot(best$hidden, aes(x, y)) +
+  geom_raster(aes(fill = pred)) +
+  geom_point(aes(shape = class), data = w) +
+  sqr +
+  facet_grid(. ~ node) +
+  hidden_fill +
+  nolab
+ggsave("6-nnet/nnet-hidden-fill.pdf", width = 12, height = 3)
 
-ggsave(file = "nnet-hidden-fill.pdf", width=12, height=3)
-
-ggplot(data=best$hidden, aes(x, y)) + 
-  geom_contour(aes(z=pred, group=node), colour="grey50", size=2, breaks = 0.5) +
-  geom_point(aes(colour=class, shape=class), data=w) + 
+ggplot(best$hidden, aes(x, y)) +
+  geom_contour(aes(z = pred, group = node), colour = "grey50", size = 2,
+    breaks = 0.5) +
+  geom_point(aes(colour = class, shape = class), data = w) +
   sqr + col_d + nolab
-
-ggsave(file = "nnet-hidden-contour.pdf", width=6, height=5)
-
+ggsave("6-nnet/nnet-hidden-contour.pdf", width = 6, height = 5)
 
 # Compare fits with different numbers of nodes -------------------------------
 
 qual <- unique(many[, c("value", "accuracy", "nodes", "id")])
-qplot(accuracy, value, data=qual, xlab="", ylab="Value of fitting criterion", facets = . ~ nodes)
-ggsave(file="nnet-value-accuracy.pdf", width = 8, height = 3)
+qplot(accuracy, value, data = qual, xlab = "",
+  ylab = "Value of fitting criterion", facets = . ~ nodes)
+ggsave("6-nnet/nnet-value-accuracy.pdf", width = 8, height = 3)
 
-qplot(accuracy, data=qual, geom="histogram", facets = . ~ nodes, binwidth=0.01, xlab="Prediction accuracy")
-ggsave(file="nnet-accuracy.pdf", width = 8, height = 3)
+qplot(accuracy, data = qual, geom = "histogram", facets = . ~ nodes,
+  binwidth = 0.01, xlab = "Prediction accuracy")
+ggsave("6-nnet/nnet-accuracy.pdf", width = 8, height = 3)
 
 # Display all fits on a single plot ------------------------------------------
 
@@ -71,14 +89,17 @@ all <- mapply(function(nn, i, j) {
 }, c(many2$all,many3$all,many4$all), 1:600, rep(2:4, each=200), SIMPLIFY = FALSE)
 all <- do.call("rbind", all)
 
-source("contour.r")
+source("6-nnet/contour.r")
 contours <- ddply(all, .(nodes, id), function(df) contour(df$x, df$y, df$pred))
 contours <- subset(contours, piece == 1)
 contours <- merge(contours, qual)
-contours$acc <- cut(contours$accuracy, c(0, 0.915, 0.925, 0.98, 1), labels=c("Sub-linear", "Linear", "Super-linear", "Excellent"))
+contours$acc <- cut(contours$accuracy, c(0, 0.915, 0.925, 0.98, 1),
+  labels = c("Sub-linear", "Linear", "Super-linear", "Excellent"))
 
-ggplot(contours, aes(x, y)) + sqr +  geom_path(aes(group=id), colour=alpha("grey50", 0.5)) + facet_grid(nodes ~ acc) + nolab
-
+ggplot(contours, aes(x, y)) + sqr +
+  geom_path(aes(group = id), colour = scales::alpha("grey50", 0.5)) +
+  facet_grid(nodes ~ acc) +
+  nolab
 ggsave(file = "nnet-all.png", width=8, height=6)
 
 return()
@@ -87,22 +108,22 @@ return()
 
 load("iterations.rda")
 # results <- vector("list", 100)
-# 
+#
 # results[[1]] <- gen_preds(w[,-3], w[,3], maxit = 1, size=4)
 # for(i in 2:100) {
 #   print(i)
 #   results[[i]] <- gen_preds(w[,-3], w[,3], maxit = 5, Wts=results[[i - 1]]$nn$wts, size=4)
-# } 
+# }
 
 hidden_seq <- mapply(function(nn, i) transform(nn$hidden, time = i), results, seq_along(results), SIMPLIFY = FALSE)
 hidden_seq <- do.call("rbind", hidden_seq)
 
 # Static shots
 
-ggplot(data=hidden_seq, aes(x, y)) + 
-  geom_contour(aes(z=pred, group=time, colour=time), breaks = 0.5) + 
-  sqr + 
-  facet_grid(. ~ node) + 
+ggplot(data=hidden_seq, aes(x, y)) +
+  geom_contour(aes(z=pred, group=time, colour=time), breaks = 0.5) +
+  sqr +
+  facet_grid(. ~ node) +
   scale_colour_continuous(low="#FFF7BC", high="#D95F0E") + nolab
 
 ggsave(file = "nnet-iteration.png", width=12, height=3)
